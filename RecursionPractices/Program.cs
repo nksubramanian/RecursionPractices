595f78e6b744638491c2ff11347492619671fa9e
subramanian <subramanian.kumarappan@halliburton.com>
2024-04-09 11:38:25 +0530

    reverse linked list

--- RecursionPractices/Program.cs
+++ RecursionPractices/Program.cs
@@ -7,15 +7,7 @@ link.InsertAtEnd(3);
 link.InsertAtEnd(4);
 link.InsertAtEnd(5);
 LinkListMethods.InsertAtStart(ref link,1);
-int backcount = 0;
-int count = 1;
-link.Print();
-int j = link.GetCount();
-var jjj = link.GetMiddleElement();
-var h = link.GetMiddleElement();
-link.Print();
 
-int u = 0;
 
 
 

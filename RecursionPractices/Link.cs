a9de30278a7d3095da75b0dfd06f65b28ecc0e80
subramanian <subramanian.kumarappan@halliburton.com>
2024-04-09 08:48:50 +0530

    link list complete

---
+++ RecursionPractices/Link.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionPractices
+{
+    public class Link
+    {
+        private Link _next;
+        public int _value;
+
+        public Link(int value)
+        {
+            _value = value;
+            _next = null;
+        }
+
+
+        public void AddNext(Link link)
+        {
+            _next = link;
+        }
+
+    }
+}

aa67e90fe6f73070c413a80328413d3efd3d9385
subramanian <subramanian.kumarappan@halliburton.com>
2024-04-13 17:41:31 +0530

    done

--- RecursionPractices/Permutations.cs
+++ RecursionPractices/Permutations.cs
@@ -6,9 +6,34 @@ using System.Threading.Tasks;
 
 namespace RecursionPractices
 {
+
     public static class Permutations
     {
-
+        static (int, int) GetIndex(List<int> numbers, int no, int start, int end)
+        {
+            if (numbers.Count == 0)
+                return (-1, -1);
+            if (numbers[numbers.Count - 1] <= no)
+                return (numbers.Count + 1, numbers.Count + 1);
+            if (numbers[0] > no)
+                return (-1, -1);
+            if (end == start)
+                return (end + 1, end + 1);
+            if ((end - start == 1) && no >= numbers[end])
+                return (end + 1, end + 1);
+            if ((end - start == 1) && no < numbers[start])
+                return (start - 1, start - 1);
+            if ((end - start == 1) && no >= numbers[start] && no < numbers[end])
+                return (start + 1, start + 1);
+            else
+            {
+                int newIndex = (end - start) / 2 + start;
+                if (numbers[newIndex] <= no)
+                    return GetIndex(numbers, no, newIndex, end);
+                else
+                    return GetIndex(numbers, no, start, newIndex);
+            }
+        }
         public static List<string> GenerateNPermutations(List<string> characters, int n)
         {
             if(n == 1)

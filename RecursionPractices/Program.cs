e9563774ec91b4ae16eecb7bce05f1e876dc420c
subramanian <subramanian.kumarappan@halliburton.com>
2024-04-14 12:50:15 +0530

    done

--- RecursionPractices/Program.cs
+++ RecursionPractices/Program.cs
@@ -3,21 +3,42 @@ using RecursionPractices;
 using System;
 using System.Globalization;
 using System.Linq;
+using System.Numerics;
 
-
-
-
-var jj = Enumerable.Range(1, 7).ToList();
-List<int> costs = new List<int> { 1, 1, 1, 1 };
-var subArrays = Enumerable.Range(0, costs.Count)
-    .SelectMany(skip => Enumerable.Range(1, costs.Count - skip).Select(take => (skip, take)))
-    .Where(index =>
+List<(int, int)> rec = new List<(int, int)> ();
+int K = 3;
+List<int> ans = new List<int> ();
+List<int> nums = new List<int> { 1, 2, 3, 1, 4, 5, 2, 3, 6 };
+for(int i = 0; i<=nums.Count()-K;++i)
+{
+    if(i==0)
     {
-        var subarray = costs.Skip(index.Item1).Take(index.Item2).ToList();
-        return subarray.Count(x => x == 0) == subarray.Count(x => x == 1);
+        for (int j = 0; j <= K - 1; j++)
+        {
+            rec.Add((nums[i + j], i + j));
+        }
+        rec = rec.OrderByDescending(x => x.Item1).ToList();
+        rec = rec.Where((num, index) =>
+        {
+            return Enumerable.Range(0, index).Select(x => rec[index].Item2 > rec[x].Item2)
+            .Aggregate(true, (acc, n) => acc && n);
+        }).ToList();
+        ans.Add(rec[0].Item1);
+    }
+    else
+    {
+        rec.Add((nums[i + K - 1], i + K - 1));
+        rec = rec.OrderByDescending(x => x.Item1).ToList();
+        rec = rec.Where((num, index) =>
+        {
+            return Enumerable.Range(0, index).Select(x => rec[index].Item2 > rec[x].Item2)
+            .Aggregate(true, (acc, n) => acc && n);
+        }).Where(x=>x.Item2>=i).ToList();
+        ans.Add(rec[0].Item1);
 
-    }).OrderByDescending(x => x.Item2).FirstOrDefault();
 
-//var g = subArrays.Where(subArray=> subArray.Where(i => i == 1).Count() == subArray.Where(i => i == 0).Count()).ToList();
+    }
+    
+}
 
-var hhh = 0;
+int h = 0;

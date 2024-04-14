de7d1153f938289fbf12312dba374b485aa42e48
subramanian <subramanian.kumarappan@halliburton.com>
2024-04-14 13:10:36 +0530

    working fine

--- RecursionPractices/Program.cs
+++ RecursionPractices/Program.cs
@@ -6,9 +6,9 @@ using System.Linq;
 using System.Numerics;
 
 List<(int, int)> rec = new List<(int, int)> ();
-int K = 3;
+int K = 4;
 List<int> ans = new List<int> ();
-List<int> nums = new List<int> { 1, 2, 3, 1, 4, 5, 2, 3, 6 };
+List<int> nums = new List<int> { 7,6,5,4,3,2,1 };
 for(int i = 0; i<=nums.Count()-K;++i)
 {
     if(i==0)
@@ -17,27 +17,20 @@ for(int i = 0; i<=nums.Count()-K;++i)
         {
             rec.Add((nums[i + j], i + j));
         }
-        rec = rec.OrderByDescending(x => x.Item1).ToList();
-        rec = rec.Where((num, index) =>
-        {
-            return Enumerable.Range(0, index).Select(x => rec[index].Item2 > rec[x].Item2)
-            .Aggregate(true, (acc, n) => acc && n);
-        }).ToList();
-        ans.Add(rec[0].Item1);
+
     }
     else
     {
         rec.Add((nums[i + K - 1], i + K - 1));
-        rec = rec.OrderByDescending(x => x.Item1).ToList();
-        rec = rec.Where((num, index) =>
-        {
-            return Enumerable.Range(0, index).Select(x => rec[index].Item2 > rec[x].Item2)
-            .Aggregate(true, (acc, n) => acc && n);
-        }).Where(x=>x.Item2>=i).ToList();
-        ans.Add(rec[0].Item1);
-
-
     }
+
+    rec = rec.OrderByDescending(x => x.Item1).ToList();
+    rec = rec.Where((num, index) =>
+    {
+        return Enumerable.Range(0, index).Select(x => rec[index].Item2 > rec[x].Item2)
+        .Aggregate(true, (acc, n) => acc && n);
+    }).Where(x=>x.Item2>=i).ToList();
+    ans.Add(rec[0].Item1);
     
 }
 

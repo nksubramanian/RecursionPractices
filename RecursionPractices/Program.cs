4e20facd3aff9d6d6df934b51fc237ba26556612
subramanian <subramanian.kumarappan@halliburton.com>
2024-04-14 09:27:05 +0530

    finished

--- RecursionPractices/Program.cs
+++ RecursionPractices/Program.cs
@@ -8,18 +8,15 @@ using System.Linq;
 
 
 var jj = Enumerable.Range(1, 7).ToList();
-List<int> costs = new List<int> { 1, 0, 1, 1, 1, 0, 0 };
-var hhhhhh = costs.Count(i => i == 1);
+List<int> costs = new List<int> { 1, 1, 1, 1 };
 var subArrays = Enumerable.Range(0, costs.Count)
     .SelectMany(skip => Enumerable.Range(1, costs.Count - skip).Select(take => (skip, take)))
-    .Select(index => { 
-    
-        return (index.Item1, index.Item2, costs.Skip(index.Item1).Take(index.Item2).ToList());
-
-    }).Where(x =>
+    .Where(index =>
     {
-        return x.Item3.Count(y => y == 0) == x.Item3.Count(y => y == 1);
-    }).ToList();
+        var subarray = costs.Skip(index.Item1).Take(index.Item2).ToList();
+        return subarray.Count(x => x == 0) == subarray.Count(x => x == 1);
+
+    }).OrderByDescending(x => x.Item2).FirstOrDefault();
 
 //var g = subArrays.Where(subArray=> subArray.Where(i => i == 1).Count() == subArray.Where(i => i == 0).Count()).ToList();
 
